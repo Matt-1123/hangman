4be348790bd2f9b15aabaa04ebde8641fb200c3d
Matt Russo <matthew.russo91@gmail.com>
2021-08-04 20:46:00 -0400

    organize code and add some comments

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ const gallows = {
 const randomWord = () => wordBank[Math.floor(Math.random() * wordBank.length)];
 
 const newRound = () => {
+  // New round welcome text
   console.log("\nWelcome to Hangman!\nPress ctrl+c to stop\n");
-
   if (roundNumber === 1) {
     console.log(`Round number: ${roundNumber}`);
   } else {
@@ -91,31 +91,26 @@ const newRound = () => {
     `);
   }
 
+  // Generate a random word and display hidden word under gallows
   const word = randomWord();
   const wordArray = word.split("");
 
+  // Display this round's word as blanks
   let hiddenWord = "";
   for (let letter in wordArray) {
     hiddenWord += " __ ";
   }
 
-  // let gallows = `
-  //    _______
-  //    |/    |
-  //    |    ( )
-  //    |    /|\\
-  //    |     |
-  //    |    / \\
-  // ___|___
-  // `;
-
+  // Initialize empty arrays for correct and incorrect guesses
   let correctGuesses = [];
   let incorrectGuesses = [];
 
+  // This function uses the gallows object to update the gallows display to the console
   const updateGallows = () => {
     console.log(gallows[incorrectGuesses.length]);
   };
 
+  // When invoked, this function iterates through the correctGuesses array and updates the hidden word display with either a correctly guessed letter or a blank
   const updateHiddenWord = () => {
     hiddenWord = "";
     wordArray.forEach((ltr) => {
@@ -132,11 +127,16 @@ const newRound = () => {
     if (/[a-zA-Z]/.test(userInput)) {
       letter = userInput.toLowerCase();
     } else if (/[^a-zA-Z]/.test(userInput)) {
-      // Do something here
-      console.log("This is NOT a letter");
+      console.log("Not a letter");
       guess();
     }
 
+    /* 
+    Check if the letter the player guessed has already been guessed.
+    If not, then on a correct guess, add that letter to the correctGuesses array, then check if the player has won (every letter in wordArray is found in correctGuesses). If so, update the round number and total wins, then start a new round.
+    On a correct guess where the player has not won yet, update the hidden word and then reset the 'screen' and provide a new guess prompt
+    On an incorrect guess, add the guessed letter to the incorrectGuesses array. If this is the sixth guess, the player loses - show the final gallows image and reveal the answer, then start a new round. If they haven't lost, tell them their guess is incorrect then reset the 'screen' and provide a new guess prompt.
+    */
     if (
       correctGuesses.indexOf(letter) > -1 ||
       incorrectGuesses.indexOf(letter) > -1
@@ -148,7 +148,6 @@ const newRound = () => {
     } else if (word.includes(letter)) {
       correctGuesses.push(letter);
       console.log(correctGuesses);
-      // player wins if every letter in wordArray is found in correctGuesses
       if (wordArray.every((ltr) => correctGuesses.includes(ltr))) {
         console.log("You win!!!");
         roundNumber++;
@@ -169,7 +168,7 @@ const newRound = () => {
         guess();
       } else {
         updateGallows();
-        console.log("Sorry, you lose :(");
+        console.log(`Sorry, you lose. The word was ${word.toUpperCase()}.`);
         roundNumber++;
         newRound();
       }
@@ -178,10 +177,11 @@ const newRound = () => {
     updateGallows();
   };
 
+  // Start of new round initial screen:
   updateGallows();
+  // TODO: remove line below:
   console.log(hiddenWord);
   console.log(wordArray);
-
   guess();
 };
 
